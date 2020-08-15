94ea90cc58662b8aacd3b07a52bced44336568cf
rehank8 <mohammedid7@yahoo.com>
2020-08-14 21:26:25 -0500

    testing  with git

--- Dmnerdhtml/Controllers/HomeController.cs
+++ Dmnerdhtml/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,11 +22,32 @@ namespace Dmnerdhtml.Controllers
 			return View();
 		}
 
+		[HttpGet]
 		public ActionResult Contact()
 		{
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+		[HttpPost]
+		public ActionResult Contact(FormCollection form)
+		{
+			ViewBag.Message = "Your contact page.";
+
+
+			MailAddress to = new MailAddress(ConfigurationManager.AppSettings["ToEmailContcat"]);
+			MailMessage message = new MailMessage();
+			message.To.Add(to);
+			message.Subject = "New Message From " + form["name"];
+			message.IsBodyHtml = true;
+			message.BodyEncoding = System.Text.Encoding.ASCII;
+			message.Body = "Hello Rehan,  New Messge: " + form["message"] + " from: " + form["email"] + "Phone: " + form["phone"];
+			SmtpClient smtp = new SmtpClient();
+
+
+			smtp.Send(message);
+
+			return RedirectToAction("Contact");
 		}
 
 		public ActionResult ResturantinDesplaines()
